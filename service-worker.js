67789ec8417a6a9e3ce0baa9598a75700bec139d
Sacha Elkaim <sachaelkaim87@gmail.com>
2020-04-18 20:20:39 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sachaelkaim.github.io/precache-manifest.9e573c9ef3d15cf6f692404da38f4573.js"
+  "/sachaelkaim.github.io/precache-manifest.e717ff3589bef3cfe3466b45196f0c68.js"
 );
 
 self.addEventListener('message', (event) => {
